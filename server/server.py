aac1962fb884d5341d7860735d999334563ce324
Jason Wong <j.wong.9@alumni.ubc.ca>
2018-03-10 14:23:52 -0800

    added retrieveCourseStatByCourseIDAndUserID

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, jsonify
-from courses.forms import retrieveCoursesByUserID
+from courses.forms import CourseForms
 
 app = Flask(__name__)
+courseForms = CourseForms()
 
 @app.route('/') 
 def home():
@@ -9,7 +10,11 @@ def home():
 
 @app.route('/courses', methods=['GET'])
 def courses():
-    return jsonify({'courses': retrieveCoursesByUserID(str(request.args.get('userID')))})
+    return jsonify({'courses': courseForms.retrieveCoursesByUserID(str(request.args.get('userID')))})
+
+@app.route('/coursestat', methods=['GET'])
+def coursestat():
+    return jsonify({'coursestat': courseForms.retrieveCourseStatByCourseIDAndUserID(str(request.args.get('courseID')), str(request.args.get('userID')))})
 
 if __name__ == '__main__':
     app.run(debug=True)
